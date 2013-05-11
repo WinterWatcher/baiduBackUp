7cfe5f09a5b45428c59f7dff678eaf3251576421
Steven Kong <Be@BBee.Be>
2013-05-11 19:32:22 +0800

    一个简单的备份执行文件

--- BackUp.sh
+++ BackUp.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+#一个简单的备份执行文件
 mysqldump -uroot -pyourpasswd  book > /home/backup.sql
 tar czvf /home/backup.tar.gz /home/wwwroot/* /home/backup.sql
 lynx -source http://www.youwebsite.com/xxx/xxx/BackUp.php
